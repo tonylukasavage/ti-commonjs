da1eacb27d4e2e20299af53a884174b22e1962cd
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-03-28 11:17:40 -0400

    compiling commonjs conformance tests

--- test/app/lib/modules/quux.js
+++ test/app/lib/modules/quux.js
@@ -1,3 +1,3 @@
 module.exports = function() {
-	console.log('quux');
+	return 'quux';
 };
--- test/app/lib/test/cov_test.js
+++ test/app/lib/test/cov_test.js
@@ -1,7 +1,6 @@
-console.log('module.id: ' + module.id);
-
 (function(_require, __dirname, __filename) {
 	var require = _require('ti-node-require')(__dirname);
+
 
 	var should = require('should/should');
 	require('ti-mocha');
@@ -10,13 +9,88 @@ console.log('module.id: ' + module.id);
 
 	describe('ti-node-require', function() {
 
-		it('should define module as an object', function() {
-			should.exist(module);
-			module.should.be.an.Object;
+		describe('require()', function() {
+
+			it('should be a function', function() {
+				should.exist(require);
+				require.should.be.a.Function;
+			});
+
+			it('should load modules via absolute paths', function() {
+				var quux = require('/modules/quux');
+				should.exist(quux);
+				quux.should.be.a.Function;
+				quux().should.equal('quux');
+			});
+
+		});
+
+		describe('free variables', function() {
+
+			it('"module" should be an object', function() {
+				should.exist(module);
+				module.should.be.an.Object;
+			});
+
+			it('"module.exports" should be an object', function() {
+				should.exist(module.exports);
+				module.exports.should.be.an.Object;
+			});
+
+			it('"exports" should be an object', function() {
+				should.exist(exports);
+				exports.should.be.an.Object;
+			});
+
+			it('"exports" should equal "module.exports"', function() {
+				should.exist(exports);
+				exports.should.equal(module.exports);
+			});
+
+			it('"module.id" should be the module\'s fully resolved filename', function() {
+				module.id.should.equal(__filename);
+			});
+
+			it('"module.require" should be a function', function() {
+				should.exist(module.require);
+				module.require.should.be.a.Function;
+			});
+
+			it('"module.require" should equal this module\'s "require"', function() {
+				should.exist(module.require);
+				module.require.should.equal(require);
+			});
+
+			it('"module.require" should require modules as though called from this module');
+
+			it('"module.filename" should be the module\'s fully resolved filename', function() {
+				module.id.should.equal(__filename);
+			});
+
+			it('"module.loaded" should be boolean', function() {
+				should.exist(module.loaded);
+				module.loaded.should.be.Boolean;
+			});
+
+			it('"module.parent" should be an object', function() {
+				should.exist(module.parent);
+				module.parent.should.be.an.Object;
+			});
+
+			it('"module.parent" should equal parent module object');
+
+			it('"module.children" should be an array', function() {
+				should.exist(module.children);
+				module.parent.should.be.an.Array;
+			});
+
+			it('"module.children" should contain an array of modules required from this module');
+
 		});
 
 	});
 
 	mocha.run();
+
 
 })(require, '/test', '/test/cov_test.js');
