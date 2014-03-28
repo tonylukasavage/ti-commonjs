0c738f461831c35dc12910e3af9ba003174a0a33
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-03-28 11:37:53 -0400

    don't wrap other modules yet

--- test/app/lib/modules/foo/bar.js
+++ test/app/lib/modules/foo/bar.js
@@ -1,8 +1,3 @@
-(function(_require, __dirname, __filename) {
-	var require = _require('ti-node-require')(__dirname);
-
-	module.exports = function() {
-		return __filename;
-	};
-
-})(require, '/modules/foo', '/modules/foo/bar.js');
+module.exports = function() {
+	return '/modules/foo/bar.js'; // __filename
+};
